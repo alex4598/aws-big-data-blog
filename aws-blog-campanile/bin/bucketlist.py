78455901a7da032a3dcf3e40618745651182056b
Alexandru Ast <aast@adobe.com>
2016-03-23 14:25:41 +0200

    added missing line due to copy-paste

--- aws-blog-campanile/bin/bucketlist.py
+++ aws-blog-campanile/bin/bucketlist.py
@@ -59,6 +59,7 @@ def main():
                 continue 
 
             print "%s\t%s\t%s\t%s" % (key.name.encode('utf-8'), 
+                    key.etag.replace("\"", ""),
                     key.size, parse_ts(key.last_modified))
 
             ## Log stats
